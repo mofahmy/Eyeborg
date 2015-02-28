5d2f7698cbf8d71a75f45b82e6ac95d4d68d5d92
Mo Fahmy <mo@mofahmy.com>
2015-02-28 18:20:00 -0500

    SonochromaticPlayer is functional

---
+++ src/SonochromaticPlayer.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+
+import org.jfugue.MicrotoneNotation;
+import org.jfugue.Player;
+
+public class SonochromaticPlayer {
+	
+	public static final float minFrequency = (float) 363.797; // Slightly below F#4
+	public static final float maxFrequency = (float) 717.591; // Slightly below F#5
+	
+	// Value to increment frequency for every degree we go up in the color wheel
+	private static final float increment = (maxFrequency - minFrequency) / ((float) 360); 
+	
+	private MicrotoneNotation microtoneMap;
+	private Player player;
+	
+	private double[] map; // Map of color hue to audio frequency
+	
+	public SonochromaticPlayer() {
+		player = new Player();
+		
+		microtoneMap = new MicrotoneNotation();
+		for (int hue = 0; hue < 360; hue++) {
+			double frequency = minFrequency + (hue * increment);
+			microtoneMap.put(Integer.toString(hue), frequency);
+			System.out.println("Addint microtone - Hue: " + hue + " Frequency: " + frequency);
+		}
+	}
+	
+	// Play the frequency associated with a particular hue
+	public void play(int hue) {
+		player.play(microtoneMap.getMusicString(Integer.toString(hue)));
+	}
+	
+	
+}
