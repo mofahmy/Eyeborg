13245c5b108ba1d5b0ed02ac6942d35969f72e62
Mo Fahmy <mo@mofahmy.com>
2015-03-01 19:40:51 -0500

    Trimmed unused code from ColorDetector class

--- src/ColorDetector.java
+++ src/ColorDetector.java
@@ -3,28 +3,8 @@ import java.awt.image.BufferedImage;
 
 
 public class ColorDetector {
-
-	public static Color getDominantColor(BufferedImage img) {
-		Color dominant = Color.BLACK;
-		
-		int width = img.getWidth();
-		int height = img.getHeight();
-		
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				
-				// Retrieves RGBA color components of a pixel as a TYPE_INT_ARGB
-				int pixel = img.getRGB(x, y);
-				
-				int[] rgb = getRGB(pixel);
-				
-			    //System.out.println("Pixel (" + x + "," + y + ") - Color: " + c.toString());
-			}
-		}
-		
-		return dominant;
-	}
 	
+	// Returns the hue of a pixel
 	public static int getHue(int pixel) {
 		int[] rgb = getRGB(pixel);
 		Color c = new Color(rgb[0], rgb[1], rgb[2]);
@@ -46,7 +26,7 @@ public class ColorDetector {
 	private static double[] getHSB(Color c) {
 		float[] hsb_float = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
 		double[] hsb = { (double) hsb_float[0], (double) hsb_float[1], (double) hsb_float[2] };
-		//System.out.println("Calculated hue: " + hsb[0]);
+
 		return hsb;
 	}
 }
